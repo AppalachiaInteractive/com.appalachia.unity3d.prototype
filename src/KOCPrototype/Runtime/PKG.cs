d26cf6d2e4f811706497daf789748e9a63337d31
Chris Schubert <chris.a.schubert25@gmail.com>
2021-11-03 13:46:59 -0400

    Committing before testing the package v. repo switch

--- src/KOCPrototype/Runtime/PKG.cs
+++ src/KOCPrototype/Runtime/PKG.cs
@@ -8,7 +8,7 @@ namespace Appalachia.Prototype.KOCPrototype
 {
     internal static partial class PKG
     {
-        public const int Priority = -281000;
+        public const int Priority = -283000;
         public const string Name = "Prototype/KOCPrototype";
         public const string Prefix = Root + Name + "/";
         public const string Root = "Appalachia/";
